60827d772e53221a4c14c424774fbcb00ae48125
jsala <javier.salamanca.munoz@gmail.com>
2018-04-10 19:45:20 +0200

    + Se construye la parte nativa a través de gradle. + Ya no se usa el script old-ndk-build.sh.

--- OsmAnd/jni/Application.mk
+++ OsmAnd/jni/Application.mk
@@ -5,8 +5,7 @@ APP_SHORT_COMMANDS := true
 # Specify least supported Android platform version
 APP_PLATFORM := android-16
 
-APP_ABI := x86
-APP_ABI += armeabi-v7a
+APP_ABI := x86 armeabi-v7a
     
 ifndef OSMAND_DEBUG_NATIVE
     # Force release compilation in release optimizations, even if application is debuggable by manifest
